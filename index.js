b909e800499f8e8e0365f09675366135f77a016b
seanlancaster <n43s@hotmail.com>
2017-08-27 20:59:41 -0700

    Adding config.json and SQLite
    Removal of token from code and adding the basis for storing settings in SQLite db

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const Commando = require('discord.js-commando');
+const config = require("./config.json");
+const sqlite = require('sqlite');
+const path = require('path');
+
 const client = new Commando.Client({
     owner: '349096456348499968'
+});
+
+sqlite.open(path.join(__dirname, "settings.sqlite3")).then(db => {
+    client.setProvider(new Commando.SQLiteProvider(db));
 });
 
 client.registry.registerGroup('random', 'Random');
 client.registry.registerDefaults();
 client.registry.registerCommandsIn(__dirname + '/commands');
 
-client.login('MzQ5MDk2NDU2MzQ4NDk5OTY4.DHwhCw.MTYAiNDo9pcjfTjoTAzF3mxPE7s');
+client.login(config.token);
